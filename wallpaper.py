784b8b3a78c39475320385053b2fc93a1f069e3b
AngryPoatt0 <211957142@qq.com>
2020-10-11 18:55:32 +0800

    nothing

--- wallpaper.py
+++ wallpaper.py
@@ -6,7 +6,10 @@ import sys
 
 command = sys.argv
 path = "C:\\Users\\dell\\Desktop\\wh"
-file = os.listdir(path)
-filepath = path + "\\" + random.choice(file)
-print(filepath)
-ctypes.windll.user32.SystemParametersInfoW(20, 0, filepath, 0)
+if(len(command) == 1):
+    file = os.listdir(path)
+    filepath = path + "\\" + random.choice(file)
+    print("wallpaper change to:"filepath)
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, filepath, 0)
+else:
+    pass
